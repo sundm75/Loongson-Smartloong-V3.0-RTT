167ae48f4098f73f5565e785eec2bc4deabb8b1b
sundm75 <sundm75@163.com>
2018-02-09 14:03:24 +0800

    修改头文件名

--- bsp/ls1cdev/drivers/drv_can.c
+++ bsp/ls1cdev/drivers/drv_can.c
@@ -27,15 +27,15 @@
 #include <board.h>
 #include <rthw.h>
 
-#include <Drv_can.h>
+#include <drv_can.h>
 
 
 #include "ls1c.h"
-#include "ls1c_public.h"
-#include "ls1c_regs.h"
-#include "ls1c_clock.h"
-#include "ls1c_can.h"
-#include "ls1c_pin.h"
+#include "../libraries/ls1c_public.h"
+#include "../libraries/ls1c_regs.h"
+#include "../libraries/ls1c_clock.h"
+#include "../libraries/ls1c_can.h"
+#include "../libraries/ls1c_pin.h"
 
 
 #ifdef RT_USING_CAN
